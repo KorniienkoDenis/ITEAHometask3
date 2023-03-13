041d6559b5b8cd6f6ab0cac57e45f1bbc49b546d
Denis Korniienko <kubikrubikmegaminks@gmail.com>
2023-03-13 07:55:52 +0300

    done: ITEAHometask3 completed

--- Task/Task/Task.cpp
+++ Task/Task/Task.cpp
@@ -1,20 +1,107 @@
-// Task.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+/*
+	
+	Task.
+	Write a program which output in the console a
+	pseudographic image (you can take any image).
+	For drawing a figure applly any symbol.
+		
+
+	This is how the drawing will turn out:
+
+		            @
+		           @@@
+		          @@@@@
+	             @@@@@@@
+	            @@@@@@@@@
+	            =========
+	            =========
+	          &&&&&&&&&&&&&
+             &&&&&&&&&&&&&&&
+            &&&&&&&&&&&&&&&&&
+           &&&&&&&&&&&&&&&&&&&
+          &&&&&&&&&&&&&&&&&&&&&
+	           !!!!!!!!!!!
+	           !!!!! !!!!!
+	           !!!!   !!!!
+	           !!!     !!!
+	           !!       !!
+
+*/
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	for (int index = 1; index <= 5; index++)
+	{
+		for (int j = 1; j <= (10 - ((2 * index - 1)) / 2); j++)
+			std::cout << " ";
+		for (int j = 1; j <= (2 * index - 1); j++)
+			std::cout << "@";
+		for (int j = 1; j <= (10 - ((2 * index - 1)) / 2); j++)
+			std::cout << " ";
+		
+		std::cout << std::endl;
+	}
+
+	for(int index = 1; index <= 1; index++)
+	{
+		for (int j = 1; j <= (6 - ((2 * index - 1)) / 2); j++)
+			std::cout << " ";
+		for (int j = 1; j <= (8 + (2 * index - 1)); j++)
+			std::cout << "=";
+
+		std::cout << std::endl;
+	}
+
+	for (int index = 1; index <= 1; index++)
+	{
+		for (int j = 1; j <= (6 - ((2 * index - 1)) / 2); j++)
+			std::cout << " ";
+		for (int j = 1; j <= (8 + (2 * index - 1)); j++)
+			std::cout << "=";
+
+		std::cout << std::endl;
+	}
+
+	for (int index = 1; index <= 5; index++)
+	{
+		for (int j = 1; j <= (4 - ((2 * index - 1)) / 2); j++)
+			std::cout << " ";
+		for (int j = 1; j <= (10 + (2 * index - 1) / 2); j++)
+			std::cout << "&";
+		for (int j = 1; j <= (3 + ((2 * index - 1)) / 2); j++)
+			std::cout << "&";
+
+		std::cout << std::endl;
+	}
+
+	for (int index = 1; index <= 1; index++)
+	{
+		for (int j = 1; j <= (5 + (2 * index - 1) / 2); j++)
+			std::cout << " ";
+		for (int j = 1; j <= (11 + (2 * index - 1) / 2); j++)
+			std::cout << "!";
+		std::cout << std::endl;
+	}
+
+	for (int index = 1; index <= 4; index++)
+	{
+		for (int j = 1; j <= 5; j++)
+			std::cout << " ";
+
+		for (int j = 1; j <= (5 - (2 * index - 1) / 2); j++)
+			std::cout << "!";
+
+		for (int j = 1; j <= 1; j++)
+			for (int k = 1; k <= (0 + (2 * index - 1)); k++)
+				std::cout << " ";
+
+		for (int j = 1; j <= (5 - (2 * index - 1) / 2); j++)
+			std::cout << "!";
+
+		std::cout << std::endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
